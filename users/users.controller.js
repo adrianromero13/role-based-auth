f2076caf76278c5e3b874c7ae79d2522e73b4826
Adrian Romero <radrian031613@gmail.com>
2020-11-11 15:03:12 -0800

    set up controller functions with protected routes

--- users/users.controller.js
+++ users/users.controller.js
@@ -16,3 +16,43 @@ router.post('/authenticate', authenticate);
 
 // controller functions
 
+function authenticate(req, res, next) {
+  userService.authenticate(req.body)
+    .then(user => user
+      ? res.json(user)
+      : res.status(400).json({
+        message: 'Username or password is incorrect'
+      }))
+    .catch(err => next(err));
+}
+
+function getAll(req, res, next) {
+  userService.getAll()
+    .then(users => res.json(users))
+    .catch(err => next(err));
+}
+
+function getCurrent(req, res, next) {
+  userService.getById(req.user.sub)
+    .then(user => user
+      ? res.json(user)
+      : res.sendStatus(404))
+    .catch(err => next(err));
+}
+
+function getById(req, res, next) {
+  const currentUser = req.user;
+  const id = parseInt(req.params.id);
+
+  // only allow admins to access other user records
+  if (id !== currentUser.sub && currentUser.role !== Role.Administrator) {
+    return res.status(401).json({
+      message: 'Unauthorized'
+    });
+  }
+  userService.getById(req.params.id)
+    .then(user => user
+      ? res.json(user)
+      : res.sendStatus(404))
+    .catch(err => next(err));
+}
