0077cdb2f06024e801e2ce95f288a2ad9e01e858
Adrian Romero <radrian031613@gmail.com>
2020-11-10 16:30:10 -0800

    set up authentication middleware functions in user.servicejs

--- users/user.model.js
+++ users/user.model.js
@@ -18,6 +18,10 @@ const schema = new Schema({
     type: String,
     required: true,
   },
+  role: {
+    type: String,
+    required: true,
+  },
   createdDate: {
     type: Date,
     default: Date.now(),
